0b16d96c2c017fe94c2c2d53325b2da678a913c8
Ernie <gunning.ernie@gmail.com>
2021-07-27 12:36:42 +0200

    Adding Logger property to ICommandLineUserInterfaceSwitch

--- AgileCoding.Library.Interfaces.UserInterfaces.Console/ICommandLineUserInterfaceSwitch.cs
+++ AgileCoding.Library.Interfaces.UserInterfaces.Console/ICommandLineUserInterfaceSwitch.cs
@@ -1,9 +1,12 @@
 ﻿namespace AgileCoding.Library.Interfaces.UserInterfaces.Console
 {
     using System.Collections.Generic;
+    using AgileCoding.Library.Interfaces.Logging;
 
     public interface ICommandLineUserInterfaceSwitch : IHelpOptionCharacters, IPrefixCommandCharacters
     {
+        ILogger Logger { get; set; }
+
         List<ICommandLineUserInterface> UserInterfaces { get; set; }
 
         void RegisterUserInterface(ICommandLineUserInterface interfaceToRegister);
